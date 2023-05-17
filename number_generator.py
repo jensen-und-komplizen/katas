5f16f22d0b3adb10b1ff4d159778ed14cff79d35
mirouhh <mirouhh@googlemail.com>
2023-05-17 10:18:02 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:number_generator.py

--- number_generator.py
+++ number_generator.py
@@ -1,6 +1,11 @@
 import unittest
 
+
+def generate_number():
+    return 0
+
+
 class TestNumberGenerator(unittest.TestCase):
 
     def test_generates_integers(self):
-        self.assertTrue(generate_number() == type(int))
+        self.assertTrue(type(generate_number()) == int)
