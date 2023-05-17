5177dc681d9b1da87d37c9005c2a811480fa1118
Falk Kühnel <falk@kuehnel.eu>
2023-05-17 10:20:04 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:number_generator.py

--- number_generator.py
+++ number_generator.py
@@ -9,3 +9,6 @@ class TestNumberGenerator(unittest.TestCase):
 
     def test_generates_integers(self):
         self.assertTrue(type(generate_number()) == int)
+
+    def test_at_least_two_digits(self):
+        self.assertTrue(generate_number() > 9)
