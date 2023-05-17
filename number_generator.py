0d7655455061828349936f4c7a7520977051393d
Jakob Heine <jaheine@deloitte.de>
2023-05-17 11:09:46 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:number_generator.py

--- number_generator.py
+++ number_generator.py
@@ -10,3 +10,6 @@ class TestNumberGenerator(unittest.TestCase):
 
     def test_fizz(self):
         self.assertEqual(do_fizz_buzz(3), "fizz")
+
+    def test_buzz(self):
+        self.assertEqual(do_fizz_buzz(5), "buzz")
