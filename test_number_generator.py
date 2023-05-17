722d1bdc669e21a5574f1d2c5b59fd0da95c2765
Khushboo Kumari <78410302+kkumari21-deloitte@users.noreply.github.com>
2023-05-17 09:21:21 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:test_number_generator.py

--- test_number_generator.py
+++ test_number_generator.py
@@ -3,12 +3,17 @@ import unittest
 def divisible(n:int) -> str:
     if n % 3 == 0:
         return "fizz"
+    if n % 5 == 0:
+        return "buzz"
     return str(int)
 
 
 class TestNumberGenerator(unittest.TestCase):
-
-    def test_buzz(self):
+    def test_fizz(self):
         self.assertTrue(
             divisible(3) == "fizz"
+        )
+    def test_buzz(self):
+        self.assertTrue(
+            divisible(5) == "buzz"
         )
