fce4252b381f214e2b940958e7cfc2019d24ca5b
Jakob Heine <jaheine@deloitte.de>
2023-05-17 10:57:46 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:number_generator.py

--- number_generator.py
+++ number_generator.py
@@ -1,11 +1,11 @@
 import unittest
 
 
-def do_something():
+def do_fizz_buzz():
     pass
 
 
 class TestNumberGenerator(unittest.TestCase):
 
-    def test_something_that_can_be_done(self):
-        return True
+    def test_fizz(self):
+        self.assertEqual(do_fizz_buzz(3), "fizz")
