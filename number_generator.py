50de0e84d94ea8102d37dc4e86db57e396f17bdf
TazTornadoo <71925487+TazTornadoo@users.noreply.github.com>
2023-05-17 10:58:46 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:__pycache__/number_generator.cpython-310.pyc

--- number_generator.py
+++ number_generator.py
@@ -9,3 +9,6 @@ class TestNumberGenerator(unittest.TestCase):
 
     def test_something_that_can_be_done(self):
         return True
+
+    def test_is_fizz(self):
+        self.assertTrue(type(make_fizz()) == str)
